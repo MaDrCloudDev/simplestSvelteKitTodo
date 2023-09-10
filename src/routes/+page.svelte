b592d4e7dad6a00c889d95454bc22ae3bedd5a38
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-09-10 12:59:57 -0700

    init after finished

---
+++ src/routes/+page.svelte
@@ -0,0 +1,148 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	// Define a todo type
+	interface Todo {
+		text: string;
+		completed: boolean;
+	}
+
+	// Initialize a writable store for todos
+	let todos = writable<Todo[]>([]);
+
+	// Initialize a writable store for newTodo
+	let newTodo = '';
+
+	// Load todos from localStorage on mount
+	onMount(() => {
+		if (typeof window !== 'undefined') {
+			const storedTodos = localStorage.getItem('todos');
+			todos.set(storedTodos ? JSON.parse(storedTodos) : []);
+		}
+	});
+
+	// Add todo function
+	function addTodo() {
+		if (newTodo) {
+			todos.update((currentTodos) => {
+				const updatedTodos = [...currentTodos, { text: newTodo, completed: false }];
+				localStorage.setItem('todos', JSON.stringify(updatedTodos));
+				return updatedTodos;
+			});
+			newTodo = ''; // Clear the input after adding a todo
+		}
+	}
+
+	// Toggle completed status function
+	function toggleCompleted(index: number, completed: boolean) {
+		todos.update((currentTodos) => {
+			const updatedTodos = currentTodos.map((todo, i) =>
+				i === index ? { ...todo, completed } : todo
+			);
+			localStorage.setItem('todos', JSON.stringify(updatedTodos));
+			return updatedTodos;
+		});
+	}
+
+	// Delete todo function
+	function deleteTodo(index: number) {
+		todos.update((currentTodos) => {
+			const updatedTodos = currentTodos.filter((_, i) => i !== index);
+			localStorage.setItem('todos', JSON.stringify(updatedTodos));
+			return updatedTodos;
+		});
+	}
+</script>
+
+<div class="container">
+	<div class="todo-app">
+		<h1>Todo List</h1>
+
+		<div>
+			<input type="text" bind:value={newTodo} placeholder="New Todo" />
+			<button on:click={addTodo}>Add Todo</button>
+		</div>
+
+		{#if $todos.length > 0}
+			<ul>
+				{#each $todos as todo, index (todo.text)}
+					<li>
+						<label>
+							<input
+								type="checkbox"
+								bind:checked={todo.completed}
+								on:change={(e) => toggleCompleted(index, e.target.checked)}
+							/>
+							<span class={todo.completed ? 'completed' : ''}>{todo.text}</span>
+						</label>
+						<button class="delete" on:click={() => deleteTodo(index)}>Delete</button>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<p>No todos yet. Add one above!</p>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+	}
+
+	.todo-app {
+		max-width: 400px;
+		width: 100%;
+		background-color: #333;
+		color: #fff;
+		padding: 20px;
+		border-radius: 10px;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	}
+
+	.todo-app input[type='text'],
+	.todo-app button {
+		font-size: 16px;
+		margin: 5px;
+		padding: 10px;
+		border: none;
+		border-radius: 5px;
+		outline: none;
+	}
+
+	.todo-app button {
+		background-color: #4caf50;
+		color: #fff;
+		cursor: pointer;
+	}
+
+	.todo-app ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	.todo-app li {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 5px 0;
+		border-bottom: 1px solid #555;
+	}
+
+	.todo-app label input[type='checkbox'] {
+		margin: 0 10px 0 0;
+	}
+
+	.todo-app span.completed {
+		text-decoration: line-through;
+	}
+
+	.todo-app button.delete {
+		background-color: #ff5722;
+		margin-left: 10px;
+	}
+</style>
