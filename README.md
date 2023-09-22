277e0a91d3738e9a48ef3e743f62476c046b9c2e
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-09-21 22:04:46 -0700

    make way simpler
    doing my best not to tweak it

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # [simplestSvelteKitTodo](http://simplestsveltekittodo.madr.io/)
 
-After recently writing the [simplestJsTodo](https://simplestjstodo.vercel.app/), I couldn't help but write SvelteKit version for comparison.
+After writing my [simplestJsTodo](http://simplestjstodo.madr.io/), I couldn't help but write a bunch of todo apps in other languages as part of a series.
 
-Using Bun 1.0 just to make it extra speedy:
+This series of "simplestTodoApps" has been less than simple. The second I finish the functionality, my instinct's to center the app vertically and horizontally. To make the app symmetrical. I want to make it so you can click the "todo text" to check a todo, and not just the checkbox itself. I feel impelled to fix all the a11y errors. To make the app dark-themed. I end up adding so much CSS that it's no longer simple and I'd be better off adding Tailwind; but adding a dependency isn't exactly simple either? So I'm doing my best to rewrite it and scale it back.
 
 ```
-bun install
-bun dev
-bun build
+npm install
+npm dev
+npm build
 ```
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,157 +1,80 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { writable } from 'svelte/store';
 
-	// Define a todo type
-	interface Todo {
-		text: string;
-		completed: boolean;
-	}
+	let newTodo: string = '';
+	let todos: { text: string; status: boolean }[] = [];
 
-	// Initialize a writable store for todos
-	const todos = writable<Todo[]>([]);
-
-	// Initialize a writable store for newTodo
-	let newTodo = '';
-
-	// Load todos from localStorage on mount
 	onMount(() => {
-		if (typeof window !== 'undefined') {
-			const storedTodos = localStorage.getItem('todos');
-			todos.set(storedTodos ? JSON.parse(storedTodos) : []);
+		const savedTodosString = localStorage.getItem('todos');
+		if (savedTodosString) {
+			const savedTodos = JSON.parse(savedTodosString) as { text: string; status: boolean }[];
+			if (savedTodos) {
+				todos = savedTodos;
+			}
 		}
 	});
 
-	// Function to update todos and save to localStorage
-	function updateTodos(callback: (currentTodos: Todo[]) => Todo[]) {
-		todos.update((currentTodos) => {
-			const updatedTodos = callback(currentTodos);
-			localStorage.setItem('todos', JSON.stringify(updatedTodos));
-			return updatedTodos;
-		});
-	}
-
-	// Add todo function
-	function addTodo() {
-		if (newTodo) {
-			updateTodos((currentTodos) => [...currentTodos, { text: newTodo, completed: false }]);
-			newTodo = ''; // Clear the input after adding a todo
+	function addToList() {
+		if (newTodo.trim() !== '') {
+			todos = [...todos, { text: newTodo, status: false }];
+			newTodo = '';
+			saveTodos();
 		}
 	}
 
-	// Toggle completed status function
-	function toggleCompleted(index: number) {
-		updateTodos((currentTodos) =>
-			currentTodos.map((todo, i) => (i === index ? { ...todo, completed: !todo.completed } : todo))
-		);
+	function removeFromList(index: number) {
+		todos.splice(index, 1);
+		todos = [...todos];
+		saveTodos();
 	}
 
-	// Delete todo function
-	function deleteTodo(index: number) {
-		updateTodos((currentTodos) => currentTodos.filter((_, i) => i !== index));
-	}
-
-	// Handle key down event
-	function handleKeyDown(event: KeyboardEvent, index: number) {
-		if (event.key === 'Enter') {
-			toggleCompleted(index);
-		}
+	function saveTodos() {
+		localStorage.setItem('todos', JSON.stringify(todos));
 	}
 </script>
 
 <div class="container">
-	<div class="todo-app">
-		<h1 class="title">simplestSvelteKitTodo</h1>
-
-		<div>
-			<input type="text" bind:value={newTodo} placeholder="New Todo" />
-			<button on:click={addTodo}>Add Todo</button>
-		</div>
-
-		{#if $todos.length > 0}
-			<ul>
-				{#each $todos as todo, index (todo.text)}
-					<li>
-						<label>
-							<input
-								type="checkbox"
-								checked={todo.completed}
-								on:change={() => toggleCompleted(index)}
-							/>
-							<span class={todo.completed ? 'completed' : ''}>{todo.text}</span>
-						</label>
-						<button class="delete" on:click={() => deleteTodo(index)}>Delete</button>
-					</li>
-				{/each}
-			</ul>
-		{/if}
+	<h1>simplestSvelteTodo</h1>
+	<div>
+		<input bind:value={newTodo} type="text" placeholder="Add todo..." />
+		<button on:click={addToList}>Add</button>
+	</div>
+	<div>
+		{#each todos as item, index}
+			<div>
+				<input bind:checked={item.status} type="checkbox" />
+				<span class:checked={item.status}>{item.text}</span>
+				<button
+					class="remove_button"
+					on:click={() => removeFromList(index)}
+					aria-label="Remove todo">❌</button
+				>
+			</div>
+			<br />
+		{/each}
 	</div>
 </div>
 
 <style>
 	.container {
 		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 90vh;
 		justify-content: center;
-		align-items: center;
-		min-height: 100vh;
 	}
-
-	.todo-app {
-		max-width: 400px;
-		width: 100%;
-		text-align: center;
-		background-color: #333;
-		color: #fff;
-		padding: 20px;
-		border-radius: 10px;
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	input,
+	button {
+		cursor: pointer;
 	}
-
-	.todo-app input[type='text'],
-	.todo-app button {
-		font-size: 16px;
-		margin: 5px;
-		padding: 10px;
+	input {
+		margin-bottom: 8px;
+	}
+	.remove_button {
+		background-color: transparent;
 		border: none;
-		border-radius: 5px;
-		outline: none;
 	}
-
-	.todo-app button {
-		background-color: #4caf50;
-		color: #fff;
-		cursor: pointer;
-	}
-
-	.todo-app ul {
-		list-style-type: none;
-		padding: 0;
-	}
-
-	.todo-app li {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 5px 0;
-		border-bottom: 1px solid #555;
-	}
-
-	.todo-app label input[type='checkbox'] {
-		float: left;
-		margin: 3px 10px 0 0;
-	}
-
-	.todo-app label {
-		width: 100%;
-		cursor: pointer;
-	}
-
-	.todo-app span.completed {
+	.checked {
 		text-decoration: line-through;
-	}
-
-	.todo-app button.delete {
-		background-color: #ff5722;
-		margin-left: 10px;
 	}
 </style>
